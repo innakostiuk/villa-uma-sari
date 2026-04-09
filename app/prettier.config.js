94b5c51909fa7517fa1f7dab682f155f86832b3e
Inna Kostiuk <kostiuk.inna@gmail.com>
2026-04-09 13:02:49 +0800

    add project setup

---
+++ app/prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 400,
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  prettierPlugin.configs.recommended,
   // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
